9c71578d7a7ad759409d3fa955e2370170caef47
rladuddbs <duddbs442@naver.com>
2023-09-21 12:16:53 +0900

    사각형 밑 부분 움직임 추가

--- character_grass.py
+++ character_grass.py
@@ -8,20 +8,23 @@ cx, cy = 400, 300
 def run_circle():
     print('CIRCLE')
     r = 200
-    for deg in range(0,360,5):
+    for deg in range(0,270,5):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        delay(0.1)
+        delay(0.05)
 
 
 def run_rectangle():
     print('RECTANGLE')
-    pass
-
-
+    for x in range(50,750+1,5): 
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        delay(0.05)
+    
 open_canvas()
 
 character = load_image("character.png")
