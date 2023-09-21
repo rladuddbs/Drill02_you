ae7de25858d635d5a043338d24a811720315e46d
rladuddbs <duddbs442@naver.com>
2023-09-21 11:53:56 +0900

    Drill02 프레임 작동 확인

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,28 @@
 from pico2d import *
 
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+
 open_canvas()
 
-# fill here
+character = load_image("character.png")
+grass = load_image("grass.png")
+
+
+
+while(True):
+    run_circle()
+    run_rectangle()
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
+    delay(1)
 
 close_canvas()
