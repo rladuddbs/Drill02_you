fd550364b09b723160e369e97a99ba26623790c9
rladuddbs <duddbs442@naver.com>
2023-09-21 12:33:08 +0900

    좌표 체크 완료

--- character_grass.py
+++ character_grass.py
@@ -10,12 +10,12 @@ def render_frame(x,y):
     clear_canvas_now()
     grass.draw_now(400,30)
     character.draw_now(x,y)
-    delay(0.05)
+    delay(0.001)
 
 def run_circle():
     print('CIRCLE')
     r = 200
-    for deg in range(0,270,5):
+    for deg in range(0,270,1):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
         render_frame(x,y)
@@ -23,9 +23,15 @@ def run_circle():
 
 def run_rectangle():
     print('RECTANGLE')
-    for x in range(50,750+1,5): 
+    x = 400
+    # bottom line
+    for x in range(400, 750 + 1, 1): 
         render_frame(x,90)
-    
+
+    # top line
+    for x in range(750, 50 - 1, -1): 
+        render_frame(x,550)
+        
 open_canvas()
 
 character = load_image("character.png")
